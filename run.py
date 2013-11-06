06f16de901b8943a432b20248e96208d3543f5a8
joytafty <joytafty@gmail.com>
2013-11-05 22:52:45 -0500

    update run.py for heroku

--- run.py
+++ run.py
@@ -17,7 +17,7 @@ import urlparse
 # REDIS_URL = urlparse('redis://redistogo:15d661206506247835d3ccd45c1d0f90@beardfish.redistogo.com:10935/');
 # redis = redis.Redis(REDIS_URL);
 
-url = urlparse.urlparse(os.environ.get('REDIS_URL', 'redis://localhost:6379'));
+url = urlparse.urlparse(os.environ.get('REDIS_URL', 'redis://localhost:10935'));
 redis = redis.Redis(host=url.hostname, port=url.port, db=0, password=url.password);
 
 # Local
